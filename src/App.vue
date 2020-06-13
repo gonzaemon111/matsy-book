743646b1e2e601b770b240e069e0d2cf4021ce3b
gonzaemon <goki14914@icloud.com>
2020-06-14 00:00:23 +0900

    add App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-bottom-navigation
+      v-model="bottomNav"
+      color="primary"
+    >
+      <v-btn value="recent" to="/">
+        <span>一覧</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites" to="/add">
+        <span>追加</span>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+// import Table from '@/components/Table.vue'
+
+export default {
+  name: 'App',
+
+  // components: {
+  //   Table,
+  // },
+
+  data: () => ({
+    bottomNav: 'recent',
+  }),
+};
+</script>
