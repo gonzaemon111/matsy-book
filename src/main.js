a3142870f093c5a945c5c5da1dfa8f0b0d846033
gonzaemon <goki14914@icloud.com>
2020-06-25 00:01:39 +0900

    add main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,28 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from "firebase"
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC_gi6jHdIhHuTsfzCZWYXTQD1vsgTTlKg",
+  authDomain: "matsy-books.firebaseapp.com",
+  databaseURL: "https://matsy-books.firebaseio.com",
+  projectId: "matsy-books",
+  storageBucket: "matsy-books.appspot.com",
+  messagingSenderId: "440309958464",
+  appId: "1:440309958464:web:7944df26baea29eb1f1ca5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
